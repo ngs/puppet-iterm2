67c22a9c16ab79d888c87a1a8bf1a43ba000e3cd
Atsushi Nagase <a@ngs.io>
2014-04-27 14:13:39 +0900

    added missing colons

--- manifests/colors/tomorrow_night_eighties.pp
+++ manifests/colors/tomorrow_night_eighties.pp
@@ -5,147 +5,123 @@
 #   include iterm2::colors::tomorrow_night_eighties
 #
 class iterm2::colors::tomorrow_night_eighties(
-
   $ansi_0_color = [
     0.0,
     0.0,
     0.0
-  ]
-
+  ],
   $ansi_1_color = [
     0.9490196078431372,
     0.4666666666666667,
     0.4784313725490196
-  ]
-
+  ],
   $ansi_10_color = [
     0.6,
     0.8,
     0.6
-  ]
-
+  ],
   $ansi_11_color = [
     1.0,
     0.8,
     0.4
-  ]
-
+  ],
   $ansi_12_color = [
     0.4,
     0.6,
     0.8
-  ]
-
+  ],
   $ansi_13_color = [
     0.8,
     0.6,
     0.8
-  ]
-
+  ],
   $ansi_14_color = [
     0.4,
     0.8,
     0.8
-  ]
-
+  ],
   $ansi_15_color = [
     1.0,
     0.9999743700027466,
     0.9999912977218628
-  ]
-
+  ],
   $ansi_2_color = [
     0.6,
     0.8,
     0.6
-  ]
-
+  ],
   $ansi_3_color = [
     1.0,
     0.8,
     0.4
-  ]
-
+  ],
   $ansi_4_color = [
     0.4,
     0.6,
     0.8
-  ]
-
+  ],
   $ansi_5_color = [
     0.8,
     0.6,
     0.8
-  ]
-
+  ],
   $ansi_6_color = [
     0.4,
     0.8,
     0.8
-  ]
-
+  ],
   $ansi_7_color = [
     1.0,
     0.9999743700027466,
     0.9999912977218628
-  ]
-
+  ],
   $ansi_8_color = [
     0.0,
     0.0,
     0.0
-  ]
-
+  ],
   $ansi_9_color = [
     0.9490196078,
     0.4666666667,
     0.4784313725
-  ]
-
+  ],
   $background_color = [
     0.1764705882352941,
     0.1764705882352941,
     0.1764705882352941
-  ]
-
+  ],
   $bold_color = [
     0.8,
     0.8,
     0.8
-  ]
-
+  ],
   $cursor_color = [
     0.8,
     0.8,
     0.8
-  ]
-
+  ],
   $cursor_text_color = [
     0.1764705882,
     0.1764705882,
     0.1764705882
-  ]
-
+  ],
   $foreground_color = [
     0.8,
     0.8,
     0.8
-  ]
-
+  ],
   $selected_text_color = [
     0.8,
     0.8,
     0.8
-  ]
-
+  ],
   $selection_color = [
     0.3176470588235294,
     0.3176470588235294,
     0.3176470588235294
-  ]
-
+  ],
 ) {
-  iterm2::colors { 'Tomorrow Night Eighties'
+  iterm2::colors { 'Tomorrow Night Eighties':
     ansi_0_color        => $ansi_0_color,
     ansi_1_color        => $ansi_1_color,
     ansi_10_color       => $ansi_10_color,
